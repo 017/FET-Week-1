a6641dd6d02488ba8ac1ef1f20b03691dd6d636c
Robert Ochi <robeochi@gmail.com>
2020-10-29 13:31:54 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// // Experimenting with RNG colors and stuff for fun
+
+// function partyMode($num) {
+//   $hex_values = array(
+//     '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f');
+  
+//     $current_hex = "";
+
+//     for($i=0; $i<3; $i++)
+//     {
+//         $rand = rand(0,15);
+//         $current_hex = $hex_values[$rand];
+//     }
+
+//     return $num."% { border: #".$current_hex."; }";
+
+// }
+
+// This makes the document load first before we go looking for the .gallery-thumbnail class to attach event listeners to.
